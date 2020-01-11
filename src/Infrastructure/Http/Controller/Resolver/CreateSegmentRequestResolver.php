7b2b5938c5a30ab9457d6cd97099181394873446
Alexander <mail@kladov.xyz>
2020-01-11 14:49:13 +0300

    validation in resolvers

--- src/Infrastructure/Http/Controller/Resolver/CreateSegmentRequestResolver.php
+++ src/Infrastructure/Http/Controller/Resolver/CreateSegmentRequestResolver.php
@@ -9,9 +9,26 @@ use App\Infrastructure\Http\Request\CreateSegmentRequest;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Controller\ArgumentValueResolverInterface;
 use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 final class CreateSegmentRequestResolver implements ArgumentValueResolverInterface
 {
+    /**
+     * @var ValidatorInterface
+     */
+    private ValidatorInterface $validator;
+
+    /**
+     * CreateSegmentRequestResolver constructor.
+     * @param ValidatorInterface $validator
+     */
+    public function __construct(ValidatorInterface $validator)
+    {
+        $this->validator = $validator;
+    }
+
+
     /**
      * @inheritDoc
      */
@@ -20,11 +37,48 @@ final class CreateSegmentRequestResolver implements ArgumentValueResolverInterfa
         return CreateSegmentRequest::class === $argument->getType();
     }
 
+    private function constraint()
+    {
+        $pointConstraint = [
+            'x' => [
+                new Assert\NotNull(),
+                new Assert\NotBlank(),
+                new Assert\Type([
+                    'type' => 'float',
+                    'message' => 'Value should be float type'
+                ])
+            ],
+            'y' => [
+                new Assert\NotNull(),
+                new Assert\NotBlank(),
+                new Assert\Type([
+                    'type' => 'float',
+                    'message' => 'Value should be float type'
+                ])
+            ],
+        ];
+        return new Assert\Collection([
+            'left_side' => new Assert\Collection($pointConstraint),
+            'right_side' => new Assert\Collection($pointConstraint),
+        ]);
+    }
+
     /**
-     * @inheritDoc
+     * @param Request $request
+     * @param ArgumentMetadata $argument
+     * @return \Generator|iterable
+     * @throws ResolverError
      */
     public function resolve(Request $request, ArgumentMetadata $argument)
     {
+        $data = $request->request->all();
+        $data['left_side']['x'] = null;
+        $violations = $this->validator->validate($data, $this->constraint());
+
+        if (count($violations) > 0) {
+            throw new ResolverError($violations);
+        }
+
         $leftSideRaw = $request->request->get('left_side');
         $rightSideRaw = $request->request->get('right_side');
 
--- src/Infrastructure/Http/Controller/Resolver/PointPositionRequestResolver.php
+++ src/Infrastructure/Http/Controller/Resolver/PointPositionRequestResolver.php
@@ -9,9 +9,24 @@ use App\Infrastructure\Http\Request\PointPositionRequest;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Controller\ArgumentValueResolverInterface;
 use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 final class PointPositionRequestResolver implements ArgumentValueResolverInterface
 {
+    /**
+     * @var ValidatorInterface
+     */
+    private ValidatorInterface $validator;
+
+    /**
+     * PointPositionRequestResolver constructor.
+     * @param ValidatorInterface $validator
+     */
+    public function __construct(ValidatorInterface $validator)
+    {
+        $this->validator = $validator;
+    }
 
     /**
      * @inheritDoc
@@ -22,12 +37,34 @@ final class PointPositionRequestResolver implements ArgumentValueResolverInterfa
     }
 
     /**
-     * @inheritDoc
+     * @param Request $request
+     * @param ArgumentMetadata $argument
+     * @return \Generator|iterable
+     * @throws ResolverError
      */
     public function resolve(Request $request, ArgumentMetadata $argument)
     {
         $x1 = $request->query->get('x1');
         $y1 = $request->query->get('y1');
+
+        $pointConstraint = [
+            new Assert\NotNull(),
+            new Assert\NotBlank(),
+            new Assert\Type([
+                'type' => 'float',
+                'message' => 'Value should be float type'
+            ])
+        ];
+        $violations = $this->validator->validate(
+            new Assert\Collection([
+                'x1' => $pointConstraint,
+                'y1' => $pointConstraint
+            ])
+        );
+
+        if (count($violations) > 0) {
+            throw new ResolverError($violations);
+        }
 
         yield new PointPositionRequest(Point::create($x1, $y1));
     }
